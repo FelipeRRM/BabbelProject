9eb282e14605bc77f4e511b38d004c60ad70fc2a
FelipeRRM <felipe_rrm@hotmail.com>
2016-08-30 22:09:19 -0300

    -Created packages, added app icon and included empty SplashActivity (still has to be implemented).

---
+++ app/src/main/java/com/feliperrm/babbelproject/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.feliperrm.babbelproject.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.feliperrm.babbelproject.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
